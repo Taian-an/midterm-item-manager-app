fe30276d837808d0be5bc400f2e7c1c8dc9cea53
Taian Chen <wallenchen0202@gmail.com>
2026-01-07 10:55:39 +0700

    t

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './app.jsx'
+import app from './app.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <app />
   </StrictMode>,
 )
